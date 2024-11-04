756bfbda5f015d460de6c7ae7d648d25061d6fcd
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-11-04 13:23:39 +0000

    new changes added

--- srv/server.js
+++ srv/server.js
@@ -3,25 +3,30 @@ const cov2ap = require("@sap/cds-odata-v2-adapter-proxy");
 
 cds.on("bootstrap", (app) => {
     app.use(cov2ap());
-    app.use((req, res, next) => {
-
+    app.use( async (req, res, next) => {
      
-        // if ((req.path.includes('user_information'))) {
-        //     if (!(req.headers['authorization'] == undefined)) {
-        //         let creadentials  = Buffer.from(req.headers['authorization'].split(" ")[1],'base64').toString().split(":")
-        //         if(creadentials[0]=="saiteja" && creadentials[1] == "101010")
-        //         {
-        //             next();
-        //         }
-        //         else{
-        //             console.log("unauthorized")
-        //         }
-        //     } else {
-        //         next();
-        //     }
+        if ((req.path.includes('user_information'))) {
+            if (!(req.headers['authorization'] == undefined)) {
+                let creadentials  = Buffer.from(req.headers['authorization'].split(" ")[1],'base64').toString().split(":")
+                let get_data =  await cds.run(`select * from APP_DB_USER_DATA`)
+
+                console.log(get_data)
+                if(!(creadentials[0]=="saiteja" && creadentials[1] == "101010"))
+                {
+                    res.status(401).json({ error: "Unauthorized access" });
+                }
+                else{
+                    next();
+                }
+            } else {
+                next();
+            }
            
-        // }
-        next();
+        }
+        else {
+            next();
+        }
+      
     });
 
 
